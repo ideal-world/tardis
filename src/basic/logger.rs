3bf6268a709b75fc7390c42db88f1648b24a2b9e
gudaoxuri <i@sunisle.org>
2022-02-05 21:48:48 +0800

    Use AtomicBool instead of Mutex.

--- src/basic/logger.rs
+++ src/basic/logger.rs
@@ -1,21 +1,17 @@
-use std::sync::Mutex;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 use crate::basic::result::TardisResult;
 
-lazy_static! {
-    static ref INITIALIZED: Mutex<bool> = Mutex::new(false);
-}
+static INITIALIZED: AtomicBool = AtomicBool::new(false);
 
 pub struct TardisLogger;
 
 impl TardisLogger {
     pub(crate) fn init() -> TardisResult<()> {
-        let mut initialized = INITIALIZED.lock().unwrap();
-        if *initialized == true {
+        if INITIALIZED.swap(true, Ordering::SeqCst) {
             return Ok(());
         }
-        *initialized = true;
-
+        println!("1111111111111");
         if std::env::var_os("RUST_LOG").is_none() {
             std::env::set_var("RUST_LOG", "info");
         }
