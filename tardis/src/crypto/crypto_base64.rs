8639942ebd4a4a47fe062c9b842e53cd637d6281
gudaoxuri <i@sunisle.org>
2023-01-26 19:00:25 -0800

    Upgrade base64  usage syntax.

--- tardis/src/crypto/crypto_base64.rs
+++ tardis/src/crypto/crypto_base64.rs
@@ -1,10 +1,13 @@
+use base64::engine::general_purpose;
+use base64::Engine;
+
 use crate::basic::error::TardisError;
 use crate::basic::result::TardisResult;
 pub struct TardisCryptoBase64;
 
 impl TardisCryptoBase64 {
     pub fn decode(&self, data: &str) -> TardisResult<String> {
-        match base64::decode(data) {
+        match general_purpose::STANDARD.decode(data) {
             Ok(result) => Ok(String::from_utf8(result)?),
             Err(e) => Err(TardisError::format_error(
                 &format!("[Tardis.Crypto] Base64 decode error:{}", e),
@@ -14,6 +17,6 @@ impl TardisCryptoBase64 {
     }
 
     pub fn encode(&self, data: &str) -> String {
-        base64::encode(data)
+        general_purpose::STANDARD.encode(data)
     }
 }
--- tardis/src/web/context_extractor.rs
+++ tardis/src/web/context_extractor.rs
@@ -1,3 +1,5 @@
+use base64::engine::general_purpose;
+use base64::Engine;
 use poem::Request;
 use poem_openapi::{auth::ApiKey, SecurityScheme};
 
@@ -35,8 +37,9 @@ async fn extract_context(req: &Request) -> TardisResult<TardisContext> {
         .to_str()
         .map_err(|_| TardisError::bad_request("[Tardis.WebServer] Context header is not string", "400-tardis-webserver-context-not-str"))?;
     if !context.starts_with(TOKEN_FLAG) {
-        let context =
-            base64::decode(context).map_err(|_| TardisError::bad_request("[Tardis.WebServer] Context header is not base64", "400-tardis-webserver-context-not-base64"))?;
+        let context = general_purpose::STANDARD
+            .decode(context)
+            .map_err(|_| TardisError::bad_request("[Tardis.WebServer] Context header is not base64", "400-tardis-webserver-context-not-base64"))?;
         let context = String::from_utf8(context).map_err(|_| TardisError::bad_request("[Tardis.WebServer] Context header is not utf8", "400-tardis-webserver-context-not-utf8"))?;
         let context = TardisFuns::json
             .str_to_obj(&context)
