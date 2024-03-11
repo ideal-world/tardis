c933d92fd1defc3c806fac8c3aa17963b8a1b302
4t145 <u4t145@163.com>
2024-03-11 14:01:25 +0800

    cluster id middleware only works on cluster id is settled

--- tardis/src/web/cluster_id_mw.rs
+++ tardis/src/web/cluster_id_mw.rs
@@ -1,6 +1,8 @@
 use async_trait::async_trait;
 use poem::{http::HeaderValue, Endpoint, IntoResponse, Middleware, Request, Response};
 
+use crate::TardisFuns;
+
 pub struct AddClusterIdHeader;
 
 impl<E: Endpoint> Middleware<E> for AddClusterIdHeader {
@@ -19,11 +21,13 @@ impl<E: Endpoint> Endpoint for UniformErrorImpl<E> {
 
     async fn call(&self, req: Request) -> poem::Result<Self::Output> {
         let mut resp = self.0.call(req).await?.into_response();
-        #[cfg(feature = "cluster")]
-        {
-            let cluster_id = crate::cluster::cluster_processor::local_node_id().await;
-            if let Ok(header_value) = HeaderValue::from_str(cluster_id) {
-                resp.headers_mut().insert(TARDIS_CLUSTER_ID_HEADER, header_value);
+        if TardisFuns::fw_config_opt().is_some_and(|cfg|cfg.cluster.is_some()) {
+            #[cfg(feature = "cluster")]
+            {
+                let cluster_id = crate::cluster::cluster_processor::local_node_id().await;
+                if let Ok(header_value) = HeaderValue::from_str(cluster_id) {
+                    resp.headers_mut().insert(TARDIS_CLUSTER_ID_HEADER, header_value);
+                }
             }
         }
         Ok(resp)
